2615ccfaf815596d39a5cf9c46b58b47c132564a
Dan Korostelev <nadako@gmail.com>
2012-11-18 23:35:36 +0400

    Some docs :)

--- src/dungeons/EntityUtils.hx
+++ src/dungeons/EntityUtils.hx
@@ -5,13 +5,23 @@ import net.richardlord.ash.core.Entity;
 
 import dungeons.components.PlayerControls;
 
+/**
+ * Helper class for getting info on entities.
+ * Use with "using dungeons.EntityUtils"
+ **/
 class EntityUtils
 {
+    /**
+     * Is this entity a player?
+     **/
     public static inline function isPlayer(entity:Entity):Bool
     {
         return entity.has(PlayerControls);
     }
 
+    /**
+     * Get a sensible name string for given entity.
+     **/
     public static inline function getName(entity:Entity):String
     {
         var desc:Description = entity.get(Description);
