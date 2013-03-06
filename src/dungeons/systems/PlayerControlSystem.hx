3f17716bf56e935e5124792f9337ddc6b1fe7d32
Dan Korostelev <nadako@gmail.com>
2013-03-06 10:46:51 +0400

    First try at layered input handling, so we can do prompts and stuff.

--- src/dungeons/systems/PlayerControlSystem.hx
+++ src/dungeons/systems/PlayerControlSystem.hx
@@ -18,6 +18,7 @@ class PlayerControlSystem extends System
 {
     private var obstacleSystem:ObstacleSystem;
     private var nodeList:NodeList<PlayerActorNode>;
+    private var inputHandler:IInputHandler;
 
     public function new()
     {
@@ -28,39 +29,21 @@ class PlayerControlSystem extends System
     {
         nodeList = engine.getNodeList(PlayerActorNode);
         obstacleSystem = engine.getSystem(ObstacleSystem);
+        inputHandler = new MainInputHandler();
+        inputHandler.enter();
     }
 
     override public function removeFromEngine(engine:Engine):Void
     {
         obstacleSystem = null;
         nodeList = null;
+        inputHandler.exit();
+        inputHandler = null;
     }
 
     private function getAction(entity:Entity):Action
     {
-        var action:Action = switch (Input.lastKey)
-        {
-            case Key.UP, Key.NUMPAD_8:
-                Move(North);
-            case Key.NUMPAD_7:
-                Move(NorthWest);
-            case Key.NUMPAD_9:
-                Move(NorthEast);
-            case Key.DOWN, Key.NUMPAD_2:
-                Move(South);
-            case Key.NUMPAD_1:
-                Move(SouthWest);
-            case Key.NUMPAD_3:
-                Move(SouthEast);
-            case Key.LEFT, Key.NUMPAD_4:
-                Move(West);
-            case Key.RIGHT, Key.NUMPAD_6:
-                Move(East);
-            case Key.SPACE, Key.NUMPAD_5:
-                Wait;
-            default:
-                null;
-        }
+        var action:Action = inputHandler.processKey(Input.lastKey);
 
         if (action != null && Type.enumConstructor(action) == "Move")
             action = processMove(entity, action);
@@ -101,5 +84,157 @@ class PlayerControlSystem extends System
                 }
             }
         }
+    }
+}
+
+interface IInputHandler
+{
+    function enter():Void;
+    function exit():Void;
+    function processKey(key:Int):Action;
+}
+
+class InputHandlerBase implements IInputHandler
+{
+    private var next:IInputHandler;
+
+    public function enter():Void
+    {
+    }
+
+    public function exit():Void
+    {
+    }
+
+    public function processKey(key:Int):Action
+    {
+        if (next != null)
+            return next.processKey(key);
+        else
+            return handleKey(key);
+    }
+
+    private function pushHandler(handler:IInputHandler):Void
+    {
+        if (next != null)
+            throw "Already have next handler " + handler;
+        next = handler;
+        next.enter();
+    }
+
+    private function popHandler():Void
+    {
+        next.exit();
+        next = null;
+    }
+
+    private function handleKey(key:Int):Action
+    {
+        return null;
+    }
+}
+
+class MainInputHandler extends InputHandlerBase
+{
+    public function new()
+    {
+    }
+
+    override private function handleKey(key:Int):Action
+    {
+        var action:Action = null;
+        switch (key)
+        {
+            case Key.UP, Key.NUMPAD_8:
+                action = Move(North);
+            case Key.NUMPAD_7:
+                action = Move(NorthWest);
+            case Key.NUMPAD_9:
+                action = Move(NorthEast);
+            case Key.DOWN, Key.NUMPAD_2:
+                action = Move(South);
+            case Key.NUMPAD_1:
+                action = Move(SouthWest);
+            case Key.NUMPAD_3:
+                action = Move(SouthEast);
+            case Key.LEFT, Key.NUMPAD_4:
+                action = Move(West);
+            case Key.RIGHT, Key.NUMPAD_6:
+                action = Move(East);
+            case Key.SPACE, Key.NUMPAD_5:
+                action = Wait;
+            case Key.C:
+                pushHandler(new ChooseDirectionHandler(testChooseDir));
+        }
+        return action;
+    }
+
+    private function testChooseDir(dir:Direction):Action
+    {
+        popHandler();
+        if (dir == null)
+        {
+            trace("Direction choosing canceled");
+            return null;
+        }
+        else
+        {
+            trace("Moving to chosen direction " + dir);
+            return Move(dir);
+        }
+    }
+}
+
+typedef ChooseDirectionCallback = Direction->Action;
+
+class ChooseDirectionHandler extends InputHandlerBase
+{
+    private var cb:ChooseDirectionCallback;
+
+    public function new(cb:ChooseDirectionCallback):Void
+    {
+        this.cb = cb;
+    }
+
+    override public function enter():Void
+    {
+        trace("Choose a direction");
+    }
+
+    override public function exit():Void
+    {
+        trace("Direction chosen!");
+    }
+
+    override private function handleKey(key:Int):Action
+    {
+        if (key == Key.ESCAPE)
+            return cb(null);
+
+        var dir:Direction = switch (key)
+        {
+            case Key.UP, Key.NUMPAD_8:
+                North;
+            case Key.NUMPAD_7:
+                NorthWest;
+            case Key.NUMPAD_9:
+                NorthEast;
+            case Key.DOWN, Key.NUMPAD_2:
+                South;
+            case Key.NUMPAD_1:
+                SouthWest;
+            case Key.NUMPAD_3:
+                SouthEast;
+            case Key.LEFT, Key.NUMPAD_4:
+                West;
+            case Key.RIGHT, Key.NUMPAD_6:
+                East;
+            default:
+                null;
+        };
+        if (dir != null)
+            return cb(dir);
+        else
+            return null;
     }
 }
